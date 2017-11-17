7c62ca598c7695ffbf8553850f05a0a03701f596
Mateusz Dyrdół <mateuszdyrdol@gmail.com>
2017-11-17 14:02:32 +0100

    main fixed

--- main.cpp
+++ main.cpp
@@ -11,10 +11,6 @@ int main()
 {
 	int x;
 	test(1, 1000);
-	//std::cout << std::hex << SDBMHash("ty no nie wiem jak tam twoja szmaciura zrogowacia³a niedzwiedzica co sie kurwi pod mostem za wojaka") << std::endl;
-	//std::cout << std::hex << SDBMHash("ty no nie wiem jak tam twoja szmaciura zrogowacia³a niedziiedzica co sie kurwi pod mostem za wojaka") << std::endl;
-	//std::cout << std::hex << SDBMHash("tyjaka") << std::endl;
-	//std::cout << std::hex << SDBMHash("t") << std::endl;
 	std::cin >> x;
 	return 0;
 	
